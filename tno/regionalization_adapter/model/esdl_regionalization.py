8c8389cc39f21d755437abbb5fe74e21f86b4857
Ruduan Plug <ruduan.plug@tno.nl>
2023-05-12 15:23:04 +0200

    Post encoding fix

--- tno/regionalization_adapter/model/esdl_regionalization.py
+++ tno/regionalization_adapter/model/esdl_regionalization.py
@@ -32,29 +32,20 @@ class ESDLRegionalization(Model):
             return model_run_info
 
         config: RegionalizationAdapterConfig = self.model_run_dict[model_run_id].config
-        # path = self.process_path(config.input_esdl_file_path, config.base_path)
-
-        # input_esdl_bytes = self.load_from_minio(path, model_run_id)
-        # input_esdl = urllib.parse.quote(input_esdl_bytes.decode('utf-8'), safe='')
-
         url = config.reg_config.path + config.reg_config.endpoint
-
-        # logger.info(f"Input esdl_b64: {str(config.esdl_b64)}")
-        # logger.info(f"Input rules: {str(config.rules)}")
-        # logger.info(f"Input to_scope: {str(config.to_scope)}")
-        # logger.info(f"Input from_scope: {str(config.from_scope)}")
-        # logger.info(f"Input year: {str(config.year)}")
-
-        # headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        response = requests.post(
-            url,
-            data={
-                'esdl_b64': config.esdl_b64,
+        print(url)
+        data_post = {
+                "esdl_b64": config.esdl_b64,
                 "rules": config.rules,
                 "to_scope": config.to_scope,
                 "from_scope": config.from_scope,
                 "year": config.year
             }
+
+        print(data_post)
+        response = requests.post(
+            url,
+            json=data_post
         )
         logger.info(f"Response: {str(response)}")
 
